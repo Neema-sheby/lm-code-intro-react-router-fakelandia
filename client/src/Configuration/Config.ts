8d57471848701767d9e5d3c994b7571b02d4a947
neema <neemadas@gmail.com>
2023-02-16 20:26:05 +0000

    feat: Add variables for min-max Subject and Textarea character count

--- client/src/Configuration/Config.ts
+++ client/src/Configuration/Config.ts
@@ -1,3 +1,9 @@
 export const MISDEMEANOUR_NUM = 50;
 export const IMAGE_WIDTH = 180;
 export const IMAGE_HEIGHT = 100;
+
+export const MIN_SUB_CHAR = 10;
+export const MAX_SUB_CHAR = 50;
+
+export const MIN_TXTAREA_CHAR = 100;
+export const MAX_TXTAREA_CHAR = 200;
