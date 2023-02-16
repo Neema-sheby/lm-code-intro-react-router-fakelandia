3396e33ba4a563335c61ab18ab8de465b16fd9fe
neema <neemadas@gmail.com>
2023-02-16 20:24:35 +0000

    fix: Remove console logs

--- client/src/Components/Fetch.tsx
+++ client/src/Components/Fetch.tsx
@@ -1,6 +1,3 @@
-// Fetch Misdemeanours
-import { Misdemeanour } from "../Pages/Misdemeanour/Misdemeanours.types";
-
 const getErrorMessage = ({ message }: { message: string }) => {
   console.error(message);
 };
@@ -12,10 +9,7 @@ export const fetchData = async (url: string) => {
     if (!response.ok) throw new Error("Something went wrong in fetching data!");
 
     const { misdemeanours } = await response.json();
-    console.log(misdemeanours);
     return misdemeanours;
-
-    // setData(json.data);
   } catch (err: unknown) {
     let message: string = "unknown error";
     if (err instanceof Error) message = err.message;
