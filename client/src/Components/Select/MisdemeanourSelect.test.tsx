0748fd5418e3acf62551e55e073dd5ce635008eb
neema <neemadas@gmail.com>
2023-02-17 16:19:18 +0000

    test: Add test cases for MisdemeanourSelect component

---
+++ client/src/Components/Select/MisdemeanourSelect.test.tsx
@@ -0,0 +1,42 @@
+import { screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import MisdemeanourSelect from "./MisdemeanourSelect";
+
+it("renders the MisdemeanourSelect component", () => {
+  render(<MisdemeanourSelect setFilteredCriminals={() => {}} />);
+
+  const selectFieldMisdemeanour = screen.getByRole("combobox");
+
+  expect(selectFieldMisdemeanour).toBeInTheDocument();
+});
+
+it("passes the selected option as prop to parent component - Misdemeanours.tsx", async () => {
+  const mock = jest.fn();
+
+  render(<MisdemeanourSelect setFilteredCriminals={mock} />);
+
+  const user = userEvent.setup();
+
+  const selectFieldMisdemeanour = screen.getByRole("combobox");
+
+  //user selects an option
+  await user.selectOptions(selectFieldMisdemeanour, "rudeness 🤪");
+
+  expect(mock).toBeCalled();
+  expect(mock).toHaveBeenCalledTimes(1);
+});
+
+it("displays the selected option", async () => {
+  const mock = jest.fn();
+
+  render(<MisdemeanourSelect setFilteredCriminals={mock} />);
+
+  const user = userEvent.setup();
+
+  const selectFieldMisdemeanour = screen.getByRole("combobox");
+
+  //user selects an option
+  await user.selectOptions(selectFieldMisdemeanour, "rudeness 🤪");
+
+  expect(selectFieldMisdemeanour).toHaveValue("rudeness 🤪");
+});
