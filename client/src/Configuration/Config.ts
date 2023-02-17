1d4c7714de0f7031269cafcba6ac00c10e2cf4bd
neema <neemadas@gmail.com>
2023-02-17 14:49:06 +0000

    docs: Change max character count for subject and min charater count for textarea

--- client/src/Configuration/Config.ts
+++ client/src/Configuration/Config.ts
@@ -3,7 +3,7 @@ export const IMAGE_WIDTH = 180;
 export const IMAGE_HEIGHT = 100;
 
 export const MIN_SUB_CHAR = 10;
-export const MAX_SUB_CHAR = 50;
+export const MAX_SUB_CHAR = 30;
 
-export const MIN_TXTAREA_CHAR = 100;
+export const MIN_TXTAREA_CHAR = 20;
 export const MAX_TXTAREA_CHAR = 200;
