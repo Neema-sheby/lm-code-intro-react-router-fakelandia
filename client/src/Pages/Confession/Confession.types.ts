db986a4721ff92c07852cbbd4d0b5aaee701d62c
neema <neemadas@gmail.com>
2023-02-17 03:08:33 +0000

    fix: Change the name of export form data type

--- client/src/Pages/Confession/Confession.types.ts
+++ client/src/Pages/Confession/Confession.types.ts
@@ -1,6 +1,6 @@
 import { MisdemeanourKind } from "../Misdemeanour/Misdemeanours.types";
 
-export type ConfessionFormProperties = {
+export type ConfessionFormDataType = {
   subject: string;
   reason: string | MisdemeanourKind | "I just want to talk";
   text: string;
