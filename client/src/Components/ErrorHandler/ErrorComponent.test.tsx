2ecf31ad8ab1f41f8185b16eef4aeb3ff2eea04d
neema <neemadas@gmail.com>
2023-02-17 14:59:45 +0000

    test: Add test to check render of error component

--- client/src/Components/ErrorHandler/ErrorComponent.test.tsx
+++ client/src/Components/ErrorHandler/ErrorComponent.test.tsx
@@ -1,6 +1,21 @@
 import { screen, render, within } from "@testing-library/react";
 import ErrorComponent from "./ErrorComponent";
 
+it("renders the error component", () => {
+  render(
+    <ErrorComponent
+      errorMessage={[
+        "⛔️ Error : Field is empty !",
+        "⛔️ Error : Number of Characters must be between 10 and 50 characters !",
+      ]}
+    />
+  );
+
+  const errorMsgList = screen.getByRole("list");
+
+  expect(errorMsgList).toBeInTheDocument();
+});
+
 it("renders the error message", () => {
   render(
     <ErrorComponent
@@ -10,7 +25,6 @@ it("renders the error message", () => {
       ]}
     />
   );
-  ("⛔️ Error : Field is empty !");
   const errorMsgItems = screen.getAllByRole("listitem");
 
   expect(errorMsgItems[0]).toHaveTextContent("⛔️ Error : Field is empty !");
