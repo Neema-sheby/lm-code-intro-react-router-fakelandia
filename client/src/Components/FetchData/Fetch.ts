fd93b96ec804b8de1ce375aa53b2c3d0994b5082
neema <neemadas@gmail.com>
2023-02-17 22:14:56 +0000

    feat: Add error status checks in fetching data

--- client/src/Components/FetchData/Fetch.ts
+++ client/src/Components/FetchData/Fetch.ts
@@ -1,12 +1,27 @@
+import { ErrorMessagesAPI } from "../ErrorHandler/ErrorMessages";
+
 const getErrorMessage = ({ message }: { message: string }) => {
   console.error(message);
 };
 
-export const fetchData = async (url: string) => {
+export const fetchData = async (
+  url: string,
+  callbackError: (T: string) => void
+) => {
   try {
     const response = await fetch(url);
 
-    if (!response.ok) throw new Error("Something went wrong in fetching data!");
+    if (!response.ok) {
+      if (response.status === 500)
+        throw new Error(`${ErrorMessagesAPI.error500}`);
+      else if (response.status === 418)
+        throw new Error(`${ErrorMessagesAPI.error418}`);
+      else if (response.status === 404)
+        throw new Error(`${ErrorMessagesAPI.error404}`);
+      else {
+        throw new Error(`${ErrorMessagesAPI.errorFetch}`);
+      }
+    }
 
     const data = await response.json();
     return data;
@@ -14,5 +29,6 @@ export const fetchData = async (url: string) => {
     let message: string = "unknown error";
     if (err instanceof Error) message = err.message;
     getErrorMessage({ message });
+    callbackError(message);
   }
 };
