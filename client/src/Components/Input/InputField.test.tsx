96f7a21d388c29c5c865d1692d9c427fcfc4cff1
neema <neemadas@gmail.com>
2023-02-17 15:40:12 +0000

    test: Add test cases for input field

---
+++ client/src/Components/Input/InputField.test.tsx
@@ -0,0 +1,85 @@
+import { screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import InputField from "./InputField";
+
+import { errMsgSubject } from "../ErrorHandler/ErrorMessages";
+
+it("renders the input component", () => {
+  render(
+    <InputField
+      id="confession-subject"
+      ContainerClassName="input__confession-container"
+      label="subject"
+      value="Confession"
+      placeholder="Input"
+      onChange={(e) => {}}
+      onValidation={[]}
+    />
+  );
+
+  const subjectField = screen.getByRole("textbox");
+
+  expect(subjectField).toBeInTheDocument();
+});
+
+it("calls it's onChange function", async () => {
+  const mock = jest.fn();
+
+  render(
+    <InputField
+      id="confession-subject"
+      ContainerClassName="input__confession-container"
+      label="subject"
+      value=""
+      placeholder="Input"
+      onChange={mock}
+      onValidation={[]}
+    />
+  );
+
+  const user = userEvent.setup();
+
+  const input = screen.getByRole("textbox", { name: /subject/i });
+
+  //user enter the input
+  await user.type(input, "Confession");
+
+  expect(mock).toBeCalled();
+  expect(mock).toHaveBeenCalledTimes(10);
+});
+
+it("displays the value passed through props", () => {
+  render(
+    <InputField
+      id="confession-subject"
+      ContainerClassName="input__confession-container"
+      label="subject"
+      value="Confession"
+      placeholder="Input"
+      onChange={() => {}}
+      onValidation={[]}
+    />
+  );
+
+  const input = screen.getByRole("textbox", { name: /subject/i });
+
+  expect(input).toHaveValue("Confession");
+});
+
+it("displays the error message passed through props", () => {
+  render(
+    <InputField
+      id="confession-subject"
+      ContainerClassName="input__confession-container"
+      label="subject"
+      value=""
+      placeholder="Input"
+      onChange={() => {}}
+      onValidation={[errMsgSubject.errCharCount]}
+    />
+  );
+
+  const errorMsgItem = screen.getByRole("listitem");
+
+  expect(errorMsgItem).toHaveTextContent(errMsgSubject.errCharCount);
+});
