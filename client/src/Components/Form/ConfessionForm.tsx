cde0a1d33c5b651e1598f180029d5a48a8bf7ab2
neema <neemadas@gmail.com>
2023-02-17 03:09:36 +0000

    fix: Pass the onChange prop of select-reason to form

--- client/src/Components/Form/ConfessionForm.tsx
+++ client/src/Components/Form/ConfessionForm.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import InputField from "../Input/InputField";
 import ConfessionSelect from "../Select/ConfessionSelect";
-import { ConfessionFormProperties } from "../../Pages/Confession/Confession.types";
+import { ConfessionFormDataType } from "../../Pages/Confession/Confession.types";
 import TextareaField from "../TextArea/TextAreaField";
 import Button from "../Button";
 import { errMsgReason, ErrorLogs } from "../ErrorHandler/ErrorMessages";
@@ -11,7 +11,7 @@ import {
   validateText,
 } from "../ErrorHandler/ErrorValidation";
 
-const initialConfessionFormDetail = {
+const defaultConfessionFormData = {
   subject: "",
   reason: "",
   text: "",
@@ -24,63 +24,72 @@ const initialErrorLog: ErrorLogs = {
 };
 
 const ConfessionForm = () => {
-  const [value, setValue] = useState<ConfessionFormProperties>(
-    initialConfessionFormDetail
+  const [value, setValue] = useState<ConfessionFormDataType>(
+    defaultConfessionFormData
   );
   const [error, setError] = useState<ErrorLogs>(initialErrorLog);
+  const [submitted, setsubmitted] = useState<boolean>(false);
 
   const onSubmit = (e: React.SyntheticEvent) => {
     e.preventDefault();
+    setsubmitted(true);
+
+    //reset inputfield, selectfield and textarea
+    setValue(defaultConfessionFormData);
   };
 
   return (
-    <form className="form" onSubmit={onSubmit}>
-      <InputField
-        id="confession-subject"
-        ContainerClassName="input__confession-container"
-        label="subject"
-        value={value.subject}
-        placeholder="Input"
-        onChange={(e) => {
-          setError({ ...error, subject: validateSubject(e.target.value) });
-          setValue({ ...value, subject: e.target.value });
-        }}
-        onValidation={error.subject}
-      />
-      <ConfessionSelect
-        setReason={(reason) => {
-          setError({
-            ...error,
-            reason: validateReasonSelection(reason),
-          });
-          setValue({ ...value, reason: reason });
-        }}
-        onValidation={error.reason}
-      />
-      <TextareaField
-        id="confession-textarea"
-        ContainerClassName="=textarea__confession-container"
-        label=""
-        value={value.text}
-        placeholder="textarea"
-        onChange={(e) => {
-          setError({ ...error, text: validateText(e.target.value) });
-          setValue({ ...value, text: e.target.value });
-        }}
-        onValidation={error.text}
-      />
-      <Button
-        className="button__form"
-        name="Confess"
-        disabled={
-          error.subject.length === 0 &&
-          error.reason.length === 0 &&
-          error.text.length === 0
-            ? false
-            : true
-        }
-      />
-    </form>
+    <>
+      <form className="form" aria-label="form-confession" onSubmit={onSubmit}>
+        <InputField
+          id="confession-subject"
+          ContainerClassName="input__confession-container"
+          label="subject"
+          value={value.subject}
+          placeholder="Input"
+          onChange={(e) => {
+            setError({ ...error, subject: validateSubject(e.target.value) });
+            setValue({ ...value, subject: e.target.value });
+          }}
+          onValidation={error.subject}
+        />
+        <ConfessionSelect
+          value={value.reason}
+          onChange={(e) => {
+            setError({
+              ...error,
+              reason: validateReasonSelection(e.target.value),
+            });
+            setValue({ ...value, reason: e.target.value });
+          }}
+          onValidation={error.reason}
+        />
+        <TextareaField
+          id="confession-textarea"
+          ContainerClassName="=textarea__confession-container"
+          label=""
+          value={value.text}
+          placeholder="textarea"
+          onChange={(e) => {
+            setError({ ...error, text: validateText(e.target.value) });
+            setValue({ ...value, text: e.target.value });
+          }}
+          onValidation={error.text}
+        />
+        <Button
+          className="button__form"
+          name="Confess"
+          disabled={
+            error.subject.length === 0 &&
+            error.reason.length === 0 &&
+            error.text.length === 0
+              ? false
+              : true
+          }
+        />
+      </form>
+      <div className="form__submitted-msg">{submitted && "Message Send"}</div>
+    </>
   );
 };
 
