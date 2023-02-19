8249ed5713ee5c97427e52c95ec2973b1bed7043
neema <neemadas@gmail.com>
2023-02-19 02:45:46 +0000

    chore: Removed unused variables declared

--- client/src/Configuration/Config.ts
+++ client/src/Configuration/Config.ts
@@ -5,5 +5,4 @@ export const IMAGE_HEIGHT = 100;
 export const MIN_SUB_CHAR = 10;
 export const MAX_SUB_CHAR = 30;
 
-export const MIN_TXTAREA_CHAR = 20;
-export const MAX_TXTAREA_CHAR = 200;
+export const MIN_TXTAREA_CHAR = 2;
