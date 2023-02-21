7554f4b6294a02601d7bdd6f6fadfa6f53cade79
neema <neemadas@gmail.com>
2023-02-21 20:28:30 +0000

    style: Add hamburger menu button

--- client/src/Layout/Navigation/Navigation.tsx
+++ client/src/Layout/Navigation/Navigation.tsx
@@ -3,29 +3,37 @@ import NavItem from "./NavItem";
 
 const Nav: React.FC = () => {
   return (
-    <nav className="nav">
-      <ul className="nav__list">
-        <NavItem
-          to="/"
-          ariaLabel="home"
-          content={
-            <svg className="icon icon-home-outline">
-              <use xlinkHref="../../public/Svg/home-outline.svg#icon-home-outline"></use>
-            </svg>
-          }
-        />
-        <NavItem
-          to="/misdemeanours"
-          ariaLabel="misdemeanours"
-          content="Misdemeanours"
-        />
-        <NavItem
-          to="/confession"
-          ariaLabel="confession"
-          content="Confess To Us"
-        />
-      </ul>
-    </nav>
+    <>
+      <div className="navbtn__container">
+        <input id="closeButton" type="checkbox" className="navbtn" />
+        <label htmlFor="closeButton" className="navbtn__hamburger-box">
+          <span className="navbtn__hamburger"></span>
+        </label>
+      </div>
+      <nav className="nav">
+        <ul className="nav__list">
+          <NavItem
+            to="/"
+            ariaLabel="home"
+            content={
+              <svg className="icon icon-home-outline">
+                <use xlinkHref="../../public/Svg/home-outline.svg#icon-home-outline"></use>
+              </svg>
+            }
+          />
+          <NavItem
+            to="/misdemeanours"
+            ariaLabel="misdemeanours"
+            content="Misdemeanours"
+          />
+          <NavItem
+            to="/confession"
+            ariaLabel="confession"
+            content="Confess To Us"
+          />
+        </ul>
+      </nav>
+    </>
   );
 };
 
