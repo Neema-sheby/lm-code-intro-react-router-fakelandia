c7f6ba6f3818fbb38700ef947c3f5d757eca07c6
neema <neemadas@gmail.com>
2023-02-21 03:42:01 +0000

    fix: Chang delay in fetching data

--- server/services/generate_misdemeanours.ts
+++ server/services/generate_misdemeanours.ts
@@ -8,7 +8,7 @@ export async function generateMisdemeanours(
   number: number
 ): Promise<Array<Misdemeanour>> {
   // pretend this is a slow API we're calling, wait 0.75s
-  await sleep(50);
+  await sleep(100);
 
   let amount = number ?? 50;
 
