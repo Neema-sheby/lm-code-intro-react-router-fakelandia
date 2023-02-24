86a3f536363cd2a12740afd8709b81d193602802
neema <neemadas@gmail.com>
2023-02-24 07:46:50 +0000

    feat: Add home page links to logo

--- client/src/Layout/Header/Header.tsx
+++ client/src/Layout/Header/Header.tsx
@@ -1,21 +1,26 @@
 import Nav from "../Navigation/Navigation";
 import logo1 from "../../Images/justice.png";
 import logo2 from "../../Images/fakelandiaLogo1.png";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="header">
       <div className="header__img-box">
-        <img
-          className="header__img--small"
-          src={logo1}
-          alt="Justice Department of Fakelandia Logo"
-        />
-        <img
-          className="header__img"
-          src={logo2}
-          alt="Justice Department of Fakelandia Logo"
-        />
+        <NavLink to="/">
+          <div className="header__img-box">
+            <img
+              className="header__img--small"
+              src={logo1}
+              alt="Justice Department of Fakelandia Logo"
+            />
+            <img
+              className="header__img"
+              src={logo2}
+              alt="Justice Department of Fakelandia Logo"
+            />
+          </div>
+        </NavLink>
       </div>
 
       <Nav />
